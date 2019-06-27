5c23dfa162c65bb831eeca9a711f19b8384ba7f1
monkut <shane.cousins@gmail.com>
2019-06-27 22:39:25 +0900

    - adding start month datetime for testcase dataframe generation

--- tests/test_charts_create.py
+++ tests/test_charts_create.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 import tempfile
 from pathlib import Path
 
@@ -14,17 +15,26 @@ def test_format_to_dataframe():
     COST_JSON_FILEPATH = TEST_DATA_DIRECTORY / 'collect_account_basic_account_metrics__result.json'
     with COST_JSON_FILEPATH.open('r', encoding='utf8') as sample_data_json:
         sample_data = json.loads(sample_data_json.read())
-        df = format_to_dataframe(sample_data)
+        target_month_start = datetime.datetime(2019, 2, 1)
+        df = format_to_dataframe(sample_data, target_month_start)
+
     figure, current_cost, previous_cost = create_daily_chart_figure(df)
+
     assert figure
+    assert current_cost
+    assert previous_cost
 
 
 def test_generate_daily_chart_image():
     COST_JSON_FILEPATH = TEST_DATA_DIRECTORY / 'collect_account_basic_account_metrics__result.json'
     with COST_JSON_FILEPATH.open('r', encoding='utf8') as sample_data_json:
         sample_data = json.loads(sample_data_json.read())
-        df = format_to_dataframe(sample_data)
+
+        target_month_start = datetime.datetime(2019, 2, 1)
+        df = format_to_dataframe(sample_data, target_month_start)
+
     figure, current_cost, previous_cost = create_daily_chart_figure(df)
+
     image_buffer = generate_daily_chart_image(figure)
     with tempfile.NamedTemporaryFile() as temp:
         temp.write(image_buffer.read())
