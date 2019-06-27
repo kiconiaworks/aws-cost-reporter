a09efc40202b06826d5dcc8033a91d175a494161
monkut <shane.cousins@gmail.com>
2019-06-27 16:42:33 +0900

    adding paging support via NextPageToken

--- pacioli/collect.py
+++ pacioli/collect.py
@@ -17,6 +17,10 @@ class CostManager:
         """
         Collect basic account cost metrics
         """
+        all_results = {
+            'ResultsByTime': []
+        }
+
         response = self.ce_client.get_cost_and_usage(
             TimePeriod={"Start": start.isoformat(), "End": end.isoformat()},
             Granularity=granularity,
@@ -26,4 +30,18 @@ class CostManager:
             ],
             Metrics=["BlendedCost", "UnblendedCost", "UsageQuantity"]
         )
-        return response
+        all_results['ResultsByTime'].extend(response['ResultsByTime'])
+
+        while 'NextPageToken' in response and response['NextPageToken']:
+            response = self.ce_client.get_cost_and_usage(
+                TimePeriod={"Start": start.isoformat(), "End": end.isoformat()},
+                Granularity=granularity,
+                GroupBy=[
+                    {'Type': 'DIMENSION', 'Key': 'LINKED_ACCOUNT'},
+                    {'Type': 'DIMENSION', 'Key': 'SERVICE'}
+                ],
+                Metrics=["BlendedCost", "UnblendedCost", "UsageQuantity"],
+                NextPageToken=response['NextPageToken']
+            )
+            all_results['ResultsByTime'].extend(response['ResultsByTime'])
+        return all_results
