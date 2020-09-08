7a278462d3892972d5fb874c3ac87c0ad91d2c69
goya813 <wasabibeef@gmail.com>
2020-09-08 19:21:24 +0900

    project/service単位でコストを取得する処理を実装

--- pacioli/collect.py
+++ pacioli/collect.py
@@ -3,6 +3,7 @@ Key class for interfacing with and obtaining data from the AWS CostExplorer API
 """
 import boto3
 import datetime
+from typing import List
 
 
 class CostManager:
@@ -13,10 +14,7 @@ class CostManager:
     def __init__(self):
         self.ce_client = boto3.client('ce')
 
-    def collect_account_basic_account_metrics(self, start: datetime.date, end: datetime.date, granularity='DAILY'):
-        """
-        Collect basic account cost metrics
-        """
+    def _collect_account_cost(self, start: datetime.date, end: datetime.date, group_by: List[dict], granularity: str = 'DAILY') -> dict:
         all_results = {
             'ResultsByTime': []
         }
@@ -24,10 +22,7 @@ class CostManager:
         response = self.ce_client.get_cost_and_usage(
             TimePeriod={"Start": start.isoformat(), "End": end.isoformat()},
             Granularity=granularity,
-            GroupBy=[
-                {'Type': 'DIMENSION', 'Key': 'LINKED_ACCOUNT'},
-                {'Type': 'DIMENSION', 'Key': 'SERVICE'}
-            ],
+            GroupBy=group_by,
             Metrics=["BlendedCost", "UnblendedCost", "UsageQuantity"]
         )
         all_results['ResultsByTime'].extend(response['ResultsByTime'])
@@ -37,12 +32,27 @@ class CostManager:
             response = self.ce_client.get_cost_and_usage(
                 TimePeriod={"Start": start.isoformat(), "End": end.isoformat()},
                 Granularity=granularity,
-                GroupBy=[
-                    {'Type': 'DIMENSION', 'Key': 'LINKED_ACCOUNT'},
-                    {'Type': 'DIMENSION', 'Key': 'SERVICE'}
-                ],
+                GroupBy=group_by,
                 Metrics=["BlendedCost", "UnblendedCost", "UsageQuantity"],
                 NextPageToken=response['NextPageToken']
             )
             all_results['ResultsByTime'].extend(response['ResultsByTime'])
+
         return all_results
+
+    def collect_account_basic_account_metrics(self, start: datetime.date, end: datetime.date, granularity='DAILY') -> dict:
+        """
+        Collect basic account cost metrics
+        """
+        group_by = [
+            {'Type': 'DIMENSION', 'Key': 'LINKED_ACCOUNT'},
+            {'Type': 'DIMENSION', 'Key': 'SERVICE'},
+        ]
+        return self._collect_account_cost(start, end, group_by, granularity)
+
+    def collect_account_group_account_project(self, start: datetime.date, end: datetime.date, granularity='DAILY') -> dict:
+        group_by = [
+            {'Type': 'TAG', 'Key': 'ProjectId'},
+            {'Type': 'DIMENSION', 'Key': 'SERVICE'},
+        ]
+        return self._collect_account_cost(start, end, group_by, granularity)
