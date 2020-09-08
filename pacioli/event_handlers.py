9d080bcd28d2a243d59c5503f435a0a1a4a27147
goya813 <wasabibeef@gmail.com>
2020-09-08 19:22:26 +0900

    project/serviceで集計した結果をSlackに通知するようにした

--- pacioli/event_handlers.py
+++ pacioli/event_handlers.py
@@ -8,7 +8,7 @@ import datetime
 import json
 from pathlib import Path
 
-from .functions import prepare_daily_chart_figure, generate_daily_chart_image
+from .functions import prepare_daily_chart_figure, prepare_daily_pie_chart_figure, generate_daily_chart_image
 from .post import SlackPostManager
 
 DEFAULT_ACCOUNTID_MAPPING_FILENAME = 'accountid_mapping.json'
@@ -41,14 +41,28 @@ def post_daily_chart(event, context) -> None:
     chart_figure, current_cost, previous_cost = prepare_daily_chart_figure(now, accountid_mapping)
     percentage_change = round((current_cost / previous_cost - 1.0) * 100, 1)
 
+    logger.info('creating pie chart...')
+    pie_figure = prepare_daily_pie_chart_figure(now)
+
     logger.info('converting chart to image (png)...')
-    image_object = generate_daily_chart_image(chart_figure)
+    daily_chart_image_object = generate_daily_chart_image(chart_figure)
+    pie_chart_image_object = generate_daily_chart_image(pie_figure)
 
     logger.info('posting image to slack...')
     slack = SlackPostManager()
+
     slack.post_image_to_channel(
         channel_name=SLACK_CHANNEL_NAME,
         title=f'AWS Cost {now.month}/{now.day} ${round(current_cost, 2)} ({percentage_change}%)',
-        image_object=image_object
+        image_object=daily_chart_image_object
     )
+    logger.info('posted: daily chart')
+
+    slack.post_image_to_channel(
+        channel_name=SLACK_CHANNEL_NAME,
+        title=f'AWS Cost ProjectId/Service {now.month}/{now.day}',
+        image_object=pie_chart_image_object
+    )
+    logger.info('posted: pie chart')
+
     logger.info('posted!')
