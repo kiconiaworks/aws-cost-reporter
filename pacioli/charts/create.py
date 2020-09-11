0244c0c11adbf71040c5abb935479be8d09d46b9
monkut <shane.cousins@gmail.com>
2020-09-11 10:48:55 +0900

    GROUBY＿TAGはProjectIdに限らないので、変数名を更新（df中身を変えていないつもり。。。） :wrench: project_ids ->  groupby_tag_values :wrench: project_id -> groupby_tag_value :wrench: display_project_id ->　display_groupby_tag_value :construction_worker: 外部　groupby tag display mappingをS3に指定できるようにlocalstackとdocker-composeを導入・追加 :construction_worker: pre-commitを導入

--- pacioli/charts/create.py
+++ pacioli/charts/create.py
@@ -5,12 +5,12 @@ import datetime
 import math
 from typing import Optional, Tuple, Union
 
-import pandas as pd
 import numpy as np
-from bokeh.models import ColumnDataSource, Legend, LegendItem, LabelSet
-from bokeh.plotting import figure
+import pandas as pd
+from bokeh.layouts import Column, Row, gridplot
+from bokeh.models import ColumnDataSource, LabelSet, Legend, LegendItem
 from bokeh.palettes import brewer, magma
-from bokeh.layouts import gridplot, Row, Column
+from bokeh.plotting import figure
 from bokeh.transform import cumsum
 
 
@@ -30,27 +30,24 @@ def create_daily_chart_figure(current_month_df: pd.DataFrame, accountid_mapping:
     last_available_date = current_month_df[current_month_df[sample_accountid] == account_max_value].index.date[0]
 
     current_cost = float(current_month_df[accountids].max().sum())
-    previous_cost = float(current_month_df.loc[last_available_date]['previous_month_total'])
+    previous_cost = float(current_month_df.loc[last_available_date]["previous_month_total"])
     percentage_change = round((current_cost / previous_cost - 1.0) * 100, 1)
 
     source = ColumnDataSource(current_month_df)
 
     today = datetime.datetime.utcnow()
     previous_day = today - datetime.timedelta(days=2)
-    today_display_str = today.strftime('%Y-%m-%d')
+    today_display_str = today.strftime("%Y-%m-%d")
     f = figure(
-        title=f'AWS Cost ({today_display_str} UTC) ${round(current_cost, 2)} ({percentage_change}%)',
+        title=f"AWS Cost ({today_display_str} UTC) ${round(current_cost, 2)} ({percentage_change}%)",
         x_axis_type="datetime",
-        x_axis_label='Date',
-        y_axis_label='Cost ($)',
+        x_axis_label="Date",
+        y_axis_label="Cost ($)",
         plot_width=800,
-        plot_height=350
+        plot_height=350,
     )
-    f.title.text_font_size = '14pt'
-    f.line('date', 'previous_month_total',
-           source=source,
-           line_color='gray',
-           line_dash='dashed')
+    f.title.text_font_size = "14pt"
+    f.line("date", "previous_month_total", source=source, line_color="gray", line_dash="dashed")
 
     def stacked(df):
         df_top = df.cumsum(axis=1)
@@ -59,7 +56,7 @@ def create_daily_chart_figure(current_month_df: pd.DataFrame, accountid_mapping:
         df_stack = pd.concat([df_bottom, df_top], ignore_index=True)
         return df_stack
 
-    current_month_previous_removed = current_month_df.drop(['previous_month_total'], axis=1)
+    current_month_previous_removed = current_month_df.drop(["previous_month_total"], axis=1)
 
     areas = stacked(current_month_previous_removed)
     areas = areas.fillna(0.0)
@@ -70,16 +67,11 @@ def create_daily_chart_figure(current_month_df: pd.DataFrame, accountid_mapping:
     # - minimum colors is 3 for a given palette, support case where only 1, or 2 colors are needed
     palette_min_colors = 3
     if number_of_areas < palette_min_colors:
-        colors = brewer['Spectral'][palette_min_colors][:number_of_areas]
+        colors = brewer["Spectral"][palette_min_colors][:number_of_areas]
     else:
-        colors = brewer['Spectral'][number_of_areas]
+        colors = brewer["Spectral"][number_of_areas]
 
-    x2 = np.hstack(
-        (
-            current_month_previous_removed.index[::-1],
-            current_month_previous_removed.index
-        )
-    )
+    x2 = np.hstack((current_month_previous_removed.index[::-1], current_month_previous_removed.index))
 
     current_month_label_values = current_month_df[current_month_df.index == pd.to_datetime(last_available_date)].groupby(level=0).sum()
     names = []
@@ -88,16 +80,16 @@ def create_daily_chart_figure(current_month_df: pd.DataFrame, accountid_mapping:
 
     current_month_labels = list(sorted([(v.iloc[0], k, previous_day) for k, v in current_month_label_values.items()]))
     current_month_total = sum(v for v, account, date in current_month_labels if account.isdigit())
-    current_month_labels.append((current_month_total, 'current_month_total', previous_day))
+    current_month_labels.append((current_month_total, "current_month_total", previous_day))
 
     accountid_display_labels = {}
     for value, name, date in current_month_labels:
         account_display_name = name
         if accountid_mapping:
             account_display_name = accountid_mapping.get(name, name)
-        display_value = f'${value:>8.2f} {account_display_name}'
+        display_value = f"${value:>8.2f} {account_display_name}"
         accountid_display_labels[name] = display_value
-        if '(Total)' in display_value:
+        if "(Total)" in display_value:
             names.append(display_value)
             values.append(value)
             dates.append(date)
@@ -105,32 +97,19 @@ def create_daily_chart_figure(current_month_df: pd.DataFrame, accountid_mapping:
     legend_items = []
     for area, color in zip(areas, colors):
         accountid = areas[area].name
-        renderer = f.patch(
-            x2,
-            areas[area].values,
-            fill_color=color,
-            alpha=0.8,
-            line_color=None
-        )
+        renderer = f.patch(x2, areas[area].values, fill_color=color, alpha=0.8, line_color=None)
         label_display_text = accountid_display_labels[accountid]
-        label_item = LegendItem(
-            label=label_display_text,
-            renderers=[renderer]
-        )
+        label_item = LegendItem(label=label_display_text, renderers=[renderer])
         legend_items.append(label_item)
     legend = Legend(
         items=legend_items,
-        location='top_left',
-        label_text_font_size='8pt',
-        label_text_font='monaco',
+        location="top_left",
+        label_text_font_size="8pt",
+        label_text_font="monaco",
     )
     f.add_layout(legend)
 
-    labels_source = ColumnDataSource(data={
-        'names': names,
-        'values': values,
-        'dates': dates
-    })
+    labels_source = ColumnDataSource(data={"names": names, "values": values, "dates": dates})
 
     # adjust x offset
     x_offset = 5
@@ -138,15 +117,15 @@ def create_daily_chart_figure(current_month_df: pd.DataFrame, accountid_mapping:
         x_offset = -150
 
     labels = LabelSet(
-        x='dates',
-        y='values',
-        text='names',
-        level='glyph',
+        x="dates",
+        y="values",
+        text="names",
+        level="glyph",
         x_offset=x_offset,
         y_offset=5,
         source=labels_source,
-        render_mode='canvas',
-        text_font_size='14pt',
+        render_mode="canvas",
+        text_font_size="14pt",
     )
     f.add_layout(labels)
     f.toolbar.logo = None
@@ -160,17 +139,22 @@ def create_daily_pie_chart_figure(df: pd.DataFrame, tag_display_mapping: Optiona
     """
     if not tag_display_mapping:
         tag_display_mapping = {}
-    project_ids = set([str(x.split("/")[0]) for x in df.columns if x != "previous_month_total"])
+    groupby_tag_values = set([str(x.split("/")[0]) for x in df.columns if x != "previous_month_total"])
 
     figures = []
-    for project_id in list(project_ids):
-        df_project = df[[x for x in df.columns if x.split("/")[0] == project_id]]
+    for groupby_tag_value in list(groupby_tag_values):
+        df_project = df[[x for x in df.columns if x.split("/")[0] == groupby_tag_value]]
         project_total_cost = df_project.iloc[-1].sum()
-        display_project_id = tag_display_mapping.get(project_id, project_id)
-        p = figure(plot_height=350, title=f"{display_project_id}: ${project_total_cost:7.2f}", toolbar_location=None,
-                   tools="hover", tooltips="@projectid_service: @value")
+        display_groupby_tag_value = tag_display_mapping.get(groupby_tag_value, groupby_tag_value)
+        p = figure(
+            plot_height=350,
+            title=f"{display_groupby_tag_value}: ${project_total_cost:7.2f}",
+            toolbar_location=None,
+            tools="hover",
+            tooltips="@projectid_service: @value",
+        )
 
-        df_project.columns = [x.split('/')[1] for x in df_project.columns]
+        df_project.columns = [x.split("/")[1] for x in df_project.columns]
 
         data = df_project.iloc[-1].reset_index(name="value").rename(columns={"index": "projectid_service"})
         data = data.sort_values("value", ascending=False).head(5)
@@ -180,9 +164,17 @@ def create_daily_pie_chart_figure(df: pd.DataFrame, tag_display_mapping: Optiona
 
         data["projectid_service"] = [f"{row['projectid_service']}: ${row['value']:7.2f}" for _, row in data.iterrows()]
 
-        p.wedge(x=0, y=1, radius=0.4,
-                start_angle=cumsum("angle", include_zero=True), end_angle=cumsum("angle"),
-                line_color="white", fill_color="color", legend="projectid_service", source=data)
+        p.wedge(
+            x=0,
+            y=1,
+            radius=0.4,
+            start_angle=cumsum("angle", include_zero=True),
+            end_angle=cumsum("angle"),
+            line_color="white",
+            fill_color="color",
+            legend="projectid_service",
+            source=data,
+        )
 
         figures.append(p)
 
